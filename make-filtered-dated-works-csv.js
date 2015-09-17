6ba03d6db6c46cc821e43324d48ad7f8c1f45bcf
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-16 23:49:43 -0400

    Write script to generate a summary of the works accessioned by dept by year

---
+++ make-filtered-dated-works-csv.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var _ = require( 'lodash' );
+var csv = require( './lib/csv' );
+
+// Year: ([12][0-9][0-9][0-9]) 1### or 2###
+// Month: ([01][0-9]) 01 thru 12
+// Day: ([0123][0-9]) 01 thru 31
+var YYYY_MM_DD = /^([12][0-9][0-9][0-9])-([01][0-9])-([0123][0-9])$/;
+
+var YEAR_REGEX = /\d{4}/;
+
+console.log( 'Reading dated-works.csv...' );
+
+csv.read( __dirname + '/dated-works.csv' ).then(function( data ) {
+  var worksByYear = _.chain( data )
+    .filter(function( work ) {
+      // Remove works with no, invalid or mis-parsed year values
+      return ! ! work.year;
+    })
+    .map(function( work ) {
+      // convert dateAcquired from a Y-M-D timestamp to just a year
+      work.dateAcquired = YEAR_REGEX.exec( work.dateAcquired )[ 0 ];
+      // Rule out a special case to lump Architecture & Design - Image Archive
+      // in with Architecture & Design
+      work.department = work.department.replace( ' - Image Archive', '' );
+      return _.pick( work, [
+        'artist',
+        'department',
+        'id',
+        'untitled',
+        'dateAcquired',
+        'year'
+      ]);
+    })
+    .groupBy( 'dateAcquired' )
+    .value();
+
+  console.log( _.countBy( data, 'department' ) );
+
+  // Sorting is probably unneeded, but it's trivial for an array of years
+  // and it provides some security that the generated CSV will be ordered
+  var years = _.keys( worksByYear ).sort(function( a, b ) {
+    return +a - +b;
+  }).map(function( year ) {
+    var counts = _.reduce( worksByYear[ year ], function( memo, work ) {
+      // If the department's count exists, increment it: otherwise set it to 1
+      // to initialize the counter
+      memo[ work.department ] = memo[ work.department ] ? memo[ work.department ] + 1 : 1;
+      if ( work.untitled ) {
+        memo.untitled = memo.untitled ? memo.untitled + 1 : 1;
+      }
+      return memo;
+    }, {});
+    counts.year = year;
+    return counts;
+  });
+
+  csv.write( 'works-acquired-by-dept-by-year.csv', years, [
+    'year',
+    'Architecture & Design',
+    'Drawings',
+    'Film',
+    'Fluxus Collection',
+    'Media and Performance Art',
+    'Painting & Sculpture',
+    'Photography',
+    'Prints & Illustrated Books',
+    'untitled'
+  ]);
+});
